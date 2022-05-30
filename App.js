4c4b6f31a136f9ea4805d8022304e456d90aac45
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-30 01:23:56 -0500

    Cambiendo al hola mundo

--- App.js
+++ App.js
@@ -1,46 +1,8 @@
 const express = require('express')
-const multer  = require('multer')
-const sharp = require('sharp')
-const fs = require('fs')
+const app = express()
 
-/*cambiando la dirección donde se descargan la imagenes*/
-const storageStrategy = multer.memoryStorage()
-const upload = multer({ storage: storageStrategy})
-
-const App = express()
-
-/*Haciendo el parseo para mostrar la información undefined.*/
-App.use(express.json())
-
-/*Haciendo una petición a nuestro servidor nodejs.*/
-App.get('/', (req, res) => {
-    res.send('Hello World from heroku')
+app.get('/', function (req, res) {
+    res.send('Hello World')
 })
 
-/*Recibir imágenes en una ruta de nuestro servidor nodejs y multer.*/
-/*instalado dependencias multer para recibir imagenes*/
-/*instalando nodemon como dependencia de desarrollo y script.*/
-/*instalando sharp como dependencia para detectar las archivos de imagenes.*/
-/*Utilizando (async) para decir que es un codigo asincrono y el (await) para esperar el resultado de la promesa.*/
-/*Para obtener (resizedImageBuffer) la imagen de datos rescalada.*/
-App.post('/imagen', upload.single('imagen') , async (req, res)=>{
-    /*Enviando información desde postman*/
-    const body = req.body
-    const imagen = req.file
-    const processedImage = sharp(imagen.buffer)
-    const resizedImage = processedImage.resize(800, 200,{
-        fit: "contain",
-        background: "#FFF"
-    })
-    const resizedImageBuffer = await resizedImage.toBuffer()
-    fs.writeFileSync('nuevaruta/prueba.png', resizedImageBuffer)
-    console.log(resizedImageBuffer)
-    res.send({resizedImage: resizedImageBuffer})
-})
-
-/*Para detectar el puerto aleatorio a heroku */
-const Port = process.env.Port || 4000
-
-App.listen(Port, function(){
-    console.log('Servido Esta escuchando en el puerto', Port)
-})
+app.listen(3000)
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Servidor De Node
-Que dice hola mundo en un petición y rescala una imagen para que no se vea distorcionada.
+# Creando Servidor de Node con un hola mundo
+Primero Crear un archivo de tipo js
+
+# Iniciar el proyecto de Node JS con npm init
+luego responder las preguntas para crear esto
+{
+  "name": "nodejs_server",
+  "version": "1.0.0",
+  "description": "Este es un servidor de Nodejs",
+  "main": "App.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Melvin Ernesto",
+  "license": "ISC",
+}
+
+# Instalar Express como dependencia en el proyecto con npm install express
+Usar este comando en la console se instalan las librerias de node_modules
+y las dependencias en package.json
+"dependencies": {
+  "express": "^4.18.1"
+}
+un package-lock.json para determinar la versión de la 
+librería express.
+
+# Escribiendo la función para controlar las peticiones a la ruta principal de nuestra aplicación
+const express = require('express')
+const app = express() 
+/*Estructura de funciones en NodeJs/Javascript*/
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+/*Poner nuestra aplicación a escuchar en un puerto.*/
+app.listen(3000) 
+
+# Para enviar la respuesta al navegador
+ejecutar en la terminal el comando 
+node NombreAchivo.js luego enter para levantar el servidor
+Abri el navegador escribir http://localhost:3000/
+
