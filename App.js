0c34b593f643620af5c1561e64c83b9ea677bbfb
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-06-16 17:43:27 -0500

    Delete

--- App.js
+++
@@ -1,9 +0,0 @@
-/*Instalar Express como dependencia de el proyecto.*/
-const express = require('express')
-const app = express()
-/*Escribiendo la función para controlar las peticiones a la ruta principal de nuestra aplicación.*/
-app.get('/', (req, res) => {
-        res.send('Hello World')
-    })
-/*Poner nuestra aplicación a escuchar en un puerto.*/
-app.listen(3000)
